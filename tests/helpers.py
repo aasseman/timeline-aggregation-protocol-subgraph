5b394066266285ce53deed48dc19b76b0debfafc
carlos.vdr <carlos.vdr@semiotic.ai>
2023-10-25 13:39:19 -0600

    test: lowered max tries
    Signed-off-by: carlos.vdr <carlos.vdr@semiotic.ai>

--- tests/helpers.py
+++ tests/helpers.py
@@ -6,7 +6,7 @@ import requests
 from eth_utils.abi import collapse_if_tuple, function_abi_to_4byte_selector
 from web3 import Web3
 
-MAX_TRIES = 15
+MAX_TRIES = 5
 GATEWAY = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
 GATEWAY_PK = "4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d"
 
